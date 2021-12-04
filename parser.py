d8f046a7e5b476b7c4c02159a98f314be41489ca
Edward Terry <edward.william.terry@gmail.com>
2021-12-04 10:00:41 -0500

    Reply in thread; add formatting

--- parser.py
+++ parser.py
@@ -11,7 +11,7 @@ from slackeventsapi import SlackEventAdapter
 # https://www.youtube.com/watch?v=KJ5bFv-IRFM&list=PLzMcBGfZo4-kqyzTzJWCV6lyK-ZMYECDc
 # https://www.youtube.com/watch?v=6gHvqXrfjuo&list=PLzMcBGfZo4-kqyzTzJWCV6lyK-ZMYECDc&index=2
 
-env_path = Path('.') / '.env'
+env_path = Path('slack_url_parser') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
@@ -23,12 +23,11 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
-    channel_id = event.get('channel_id')
+    channel_id = event.get('channel')
     ts = event.get('ts')
     text = event.get('text')
     parsed = parse_text(text)
-    # client.chat_postMessage(channel=channel_id, ts=ts, text=parsed)
-    client.chat_postMessage(channel='#na', text=parsed)
+    client.chat_postMessage(channel=channel_id, thread_ts=ts, text=parsed)
 
 def parse_text(text, token = 'arxiv.org/pdf', return_prefix='https://arxiv.org/abs/'):
     # converts type from https://arxiv.org/pdf/2005.14165.pdf to https://arxiv.org/abs/2005.14165
@@ -41,10 +40,12 @@ def parse_text(text, token = 'arxiv.org/pdf', return_prefix='https://arxiv.org/a
 
         soup = BeautifulSoup(abs_html, "html.parser")
 
+        abs_url = return_prefix + paper_id
         title = soup.find("meta", property="og:title")['content']
+        title = '*' + title + '*' # Make bold
         desc = soup.find("meta", property="og:description")['content']
-        desc = desc.split('.')[0]
-        return title + '\n' + desc
+        desc = desc.split('.')[0] + '.' # Return the first sentence
+        return '\n'.join([abs_url, title, desc])
 
 if __name__=='__main__':
     app.run(debug=True)
